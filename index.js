c1c2d7b44fdebef39da03ac7edd57d662cfa4449
Morgan Atwood <atwood.morganj@gmail.com>
2020-05-07 08:52:10 -0700

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ server: 'serving data...' });
 });
 
 const PORT = process.env.PORT || 5000;
