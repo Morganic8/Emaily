d1ba1cde1acb15f73f9461c17592a433aabd4636
Morgan Atwood <atwood.morganj@gmail.com>
2020-05-12 16:10:50 -0700

    auth flow - google - mongo - heroku

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+//fire off passport
+require('./models/User');
+require('./services/passport');
+
+//mongoose options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+mongoose.connect(keys.mongoURI, options);
 
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
 
-app.get('/', (req, res) => {
-  res.send({ server: 'serving data...' });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+//google auth route
+require('./routes/authRoutes')(app);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
